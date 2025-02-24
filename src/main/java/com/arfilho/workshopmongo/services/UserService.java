67b8503c05c425bca715b4a0665979d64f73291c
ar-filho <airton.r.filho@ufv.br>
2025-02-24 17:46:28 -0300

    Insercao de usuario com POST

--- src/main/java/com/arfilho/workshopmongo/services/UserService.java
+++ src/main/java/com/arfilho/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.arfilho.workshopmongo.services;
 
 import com.arfilho.workshopmongo.domain.User;
+import com.arfilho.workshopmongo.dto.UserDTO;
 import com.arfilho.workshopmongo.exception.ObjectNotFoundException;
 import com.arfilho.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
