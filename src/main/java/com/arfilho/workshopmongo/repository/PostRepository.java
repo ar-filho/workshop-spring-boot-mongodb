fbcae85ba80e4ef441e0ba015ab4731b11e30a07
ar-filho <airton.r.filho@ufv.br>
2025-02-25 11:34:23 -0300

    Criando entity Post com User aninhado/copiado

---
+++ src/main/java/com/arfilho/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.arfilho.workshopmongo.repository;
+
+import com.arfilho.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
